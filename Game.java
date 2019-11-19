c63f362039b0e3b3c3fb95b9b6dbda123a40be0a
EnemyVayne <53927185+EnemyVayne@users.noreply.github.com>
2019-11-18 21:33:59 -0600

    Add files via upload

--- Game.java
+++ Game.java
@@ -1,102 +1,203 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Game;
 
-
 import javafx.application.Application;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.geometry.Insets;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.shape.Circle;
-import javafx.scene.paint.Color;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.shape.Rectangle;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.scene.paint.Color;
+import javafx.scene.text.FontWeight;
+import javafx.util.Duration;
 
 /**
- *
- * @author kylef
+ * The class "Game" is the overarching application, it sets the window up, and 
+ * creates the panes/scenes/stages. The game starts off by declaring global 
+ * variables to be used throughout the application.
+ * @author Kyle_Faith
  */
 public class Game extends Application
 {
-   
+
    public static final int WIDTH = 1300;
    public static final int HEIGHT = 800;
-   Color colorGrass = new Color(0,.47,0,.8);
-   Color colorRoom = Color.SIENNA;
-   Color colorFade;
-   
-   //private Handler handler;
-   Label titleLabel;
- 
+   public Color colorGrass = new Color(0, .47, 0, .8);
+   public Color colorRoom = new Color(.5, .2, .10, 1);
+   public Color colorBlack = Color.BLACK;
 
+   private Timeline loop2;
+   private Label titleLabel, titleSpace;
+   private boolean boundaryChange = false;
 
+   Player player = new Player();
 
+   /**
+    * The method opens the stage, and creates numerous different objects, which 
+    * are used to create the panes and scenes. The method starts by first 
+    * creating the stage and setting its dimensions, then moves on to creating
+    * the backgrounds for the panes. The first scene shown is the titlescreen, 
+    * which contains two labels. It then requires user input of a space to progress
+    * onto the next scene, which is the main game. The new scene has a new player
+    * which uses button commands to move. When walking out of the scene, the 
+    * player moves to the opposite side,as if he was to spawn into a new area. 
+    * @param stage The first window to be created.
+    * @throws Exception 
+    */
    @Override
    public void start( Stage stage ) throws Exception
    {
-      //handler = new Handler();
-      titleLabel = new Label("The Suave Squad");
-      titleLabel.setFont(new Font(50)); 
 
-      titleLabel.setTextFill(Color.WHITE);
-  
-      Rectangle rectangle = new Rectangle(HEIGHT, HEIGHT, WIDTH+100, WIDTH+100); 
-      rectangle.setFill(Color.BLACK); 
-      
-      
-      StackPane titlePane = new StackPane(rectangle);
-      GridPane roomPane = new GridPane();
-      
-
-
-      titlePane.setPadding(new Insets(0,0,0,0)); 
-
-      
-      
-      titlePane.getChildren().add(titleLabel);
-
-
-      roomPane.setPadding(new Insets(100,100,100,100));
-      
-
-
-      
-      
-      Scene TitleScreen = new Scene(titlePane, WIDTH, HEIGHT);
-      Scene Room = new Scene(roomPane, WIDTH, HEIGHT);
-
-      
-      
-
-
-      Room.setFill(colorRoom);
-      
-      stage.setTitle("RPG");
-      stage.setScene(TitleScreen);
-      stage.show();
       stage.setResizable(false);
-      
-      
+      stage.show();
+      stage.setX(300);
+      stage.setY(100);
+      stage.setMaxHeight(HEIGHT);
+      stage.setMaxWidth(WIDTH);
+      stage.setMinHeight(HEIGHT);
+      stage.setMinWidth(WIDTH);
 
-      
+      Rectangle backgroundTitle = new Rectangle(WIDTH, HEIGHT);
+      Rectangle backgroundHome = new Rectangle(WIDTH, HEIGHT);
+      Rectangle backgroundExplore = new Rectangle(WIDTH, HEIGHT);
+
+      StackPane rootPaneTitle = new StackPane();
+      StackPane rootPaneHome = new StackPane();
+      StackPane rootPaneExplore = new StackPane();
+
+      VBox titlePane1 = new VBox();
+      StackPane titlePane2 = new StackPane();
+
+      rootPaneTitle.getChildren().addAll(titlePane2, titlePane1);
+
+      Scene TitleScreen = new Scene(rootPaneTitle, WIDTH, HEIGHT);
+
+      titleLabel = new Label("The Suave Squad");
+      titleSpace = new Label("Please press Space to continue...");
+
+      titleLabel.setFont(Font.font("", FontWeight.BOLD, 70));
+      titleSpace.setFont(Font.font(30));
+
+      titleLabel.setUnderline(true);
+      titleLabel.setTextFill(Color.WHITE);
+      titleSpace.setTextFill(Color.WHITE);
+      titleLabel.setAlignment(Pos.CENTER);
+      titleSpace.setAlignment(Pos.CENTER);
+
+      titleSpace.setPadding(new Insets(0, 0, 0, 57.5));
+      titlePane1.setPadding(new Insets(HEIGHT / 2 - 100, WIDTH / 2 - 300,
+                                       HEIGHT / 2 - 50, WIDTH / 2 - 300));
+
+      titlePane1.getChildren().addAll(titleLabel, titleSpace);
+      titlePane2.getChildren().add(backgroundTitle);
+
+      stage.setTitle("Kyle And Wills RPG");
+
+      stage.setScene(TitleScreen);
+
+      player.setOnKeyPressed(e -> 
+              {
+                 switch ( e.getCode() )
+                 {
+                    case UP:
+                       player.moveUp();
+                       break;
+                    case DOWN:
+                       player.moveDown();
+                       break;
+                    case LEFT:
+                       player.moveLeft();
+                       break;
+                    case RIGHT:
+                       player.moveRight();
+                 }
+               });
+
+      rootPaneHome.getChildren().addAll(backgroundHome, player);
+
+      backgroundHome.setFill(colorRoom);
+
+      Scene homeScreen = new Scene(rootPaneHome, WIDTH, HEIGHT);
+
+      TitleScreen.setOnKeyPressed(e -> 
+              {
+                 switch ( e.getCode() )
+                 {
+                    case SPACE:
+                       stage.setScene(homeScreen);
+                       break;
+                 }
+               });
+
+      player.requestFocus();
+//      homeScreen.setOnKeyPressed(e -> 
+//              {                  if debugging use this to find player position
+//                 switch ( e.getCode() )
+//                 {
+//                    case A:
+//                       System.out.println("the players x position is: " +
+//                               player.getX());
+//                       System.out.println("the players y position is: " + 
+//                               player.getY());
+//                       System.out.println();
+//                       break;
+//
+//                 }
+//      });
+
+//      if ( boundaryChange )
+//      {
+//         stage.setScene(TitleScreen);
+//      }
+      loop2 = new Timeline(
+              new KeyFrame(Duration.millis(25), e -> BoundaryCheck()));
+      loop2.setCycleCount(Timeline.INDEFINITE);
+      loop2.play();
    }
-   
-       public static void main(String [] args)
-    { 
 
-       Application.launch(args);  
-    }
-   
+   /**
+    * The method checks to see if the players current coordinates are within the
+    * boundary set, and if the player moves out of the boundary, it sets the 
+    * coordinates to the opposite side.
+    */
+   public void BoundaryCheck()
+   {
+
+      int leftBoundary = 0;
+      int rightBoundary = Game.WIDTH;
+      int topBoundary = 0;
+      int bottomBoundary = Game.HEIGHT - 30;
+
+      if ( player.getX() > rightBoundary ) //boundries for the ball
+      {
+         player.setX(leftBoundary);
+         boundaryChange = true;
+      }
+      if ( player.getX() < leftBoundary ) //boundries for the ball
+      {
+         player.setX(rightBoundary);
+         boundaryChange = true;
+      }
+      if ( player.getY() < topBoundary ) //boundries for the ball
+      {
+         player.setY(bottomBoundary);
+         boundaryChange = true;
+      }
+      if ( player.getY() > bottomBoundary ) //boundries for the ball
+      {
+         player.setY(topBoundary);
+         boundaryChange = true;
+      }
+   }
+
+   public static void main( String[] args )
+   {
+      Application.launch(args);
+   }
 
 }
--- Player.java
+++ Player.java
@@ -1,22 +1,160 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Game;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.beans.property.DoubleProperty;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.util.Duration;
+
 /**
- *
- * @author kylef
+ * The class is used to create a player, and set the animation/move operations
+ * for the player. The animation is set to refresh every .1 milliseconds, and
+ * checks the method "movePlayer" every time it ticks. The ticks cause the 
+ * player to move based on the location of the center of the circle.
+ * @author KyleFaith
  */
-public class Player extends GameObject
+public class Player extends Pane
 {
 
-   public Player( int x, int y, ID id )
+   private final double radius = 30;
+   private double x = Game.WIDTH / 2 - radius, y = Game.HEIGHT / 2 - radius;
+   private double speedX = 10, speedY = 10;
+   private Circle circle = new Circle(x, y, radius);
+   private Timeline animation;
+
+   
+   /**
+    * The method is used to create a player in the shape of a circle,
+    * add the player to a pane, and create an animation for said player. 
+    */
+   public Player()
    {
-      super(x, y, id);
-      
-     
-      
+      circle.setFill(Color.GREEN);
+      circle.setStroke(Color.BLACK);
+      getChildren().add(circle);
+
+      animation = new Timeline(
+              new KeyFrame(Duration.millis(.1), e -> movePlayer()));
+      animation.setCycleCount(Timeline.INDEFINITE);
+      animation.play();
    }
+
+   /**
+    * The method is used to resume play when the game is paused.
+    */
+   public void play()
+   {
+      animation.play();
+   }
+
+   /**
+    * The method is used to pause the game, and can be started again by using 
+    * start method.
+    */
+   public void pause()
+   {
+      animation.pause();
+   }
+
+   /**
+    * Used to bind the rate of application.
+    * 
+    * @return Binding property value for rate.
+    */
+   public DoubleProperty rateProperty()
+   {
+      return animation.rateProperty();
+   }
+
+   /**
+    * The method is used to move the player north. The method works by taking a 
+    * specified speed and adding it to the coordinate of the player so it moves
+    * by the speed every tick of the animation.
+    */
+   public void moveUp()
+   {
+      y -= speedY;
+   }
+   
+   /**
+    * The method is used to move the player south. The method works by taking a 
+    * specified speed and adding it to the coordinate of the player so it moves
+    * by the speed every tick of the animation.
+    */
+   public void moveDown()
+   {
+      y += speedY;
+   }
+
+    /**
+    * The method is used to move the player west. The method works by taking a 
+    * specified speed and adding it to the coordinate of the player so it moves
+    * by the speed every tick of the animation.
+    */
+   public void moveLeft()
+   {
+      x -= speedX;
+   }
+
+    /**
+    * The method is used to move the player east. The method works by taking a 
+    * specified speed and adding it to the coordinate of the player so it moves
+    * by the speed every tick of the animation.
+    */
+   public void moveRight()
+   {
+      x += speedX;
+   }
+
+   /**
+    * The method is used to set the private variable x to a different value.
+    * @param x is a coordinate of player
+    */
+   public void setX( int x )
+   {
+      this.x = x;
+   }
+   /**
+    * The method is used to set the private variable y to a different value.
+    * @param y is a coordinate of player
+    */
+   public void setY( int y )
+   {
+      this.y = y;
+   }
+   
+   /**
+    * The method grabs the private variable x to be able to use within different
+    * classes.
+    * @return Returns the variable found in Player class.
+    */
+   public double getX()
+   {
+      return x;
+   }
+
+   /**
+    * The method grabs the private variable y to be able to use within different
+    * classes.
+    * @return Returns the variable found in Player class.
+    */
+   public double getY()
+   {
+      return y;
+   }
+
+   /**
+    * The method is used to move the players coordinates, and is the main movement
+    * command. The movePlayer method is looped through every time the animation
+    * takes place(every .1 milliseconds), to see if there is a change in position.
+    * 
+    */
+   protected void movePlayer()
+   {
+      circle.setCenterX(x);
+      circle.setCenterY(y);
+   }
+
 }
